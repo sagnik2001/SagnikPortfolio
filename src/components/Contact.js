af204b7409bf59351b697bc66877a24518e48f2c
Sagnikpal <Palsagnik2001@gmail.com>
2021-09-28 23:17:11 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ import {useForm} from "react-hook-form"
 const Contact=()=>{
   const {register,handleSubmit,formState:{errors}} =useForm()
   const onSubmitHandler=(e)=>{
-    e.preventDefault();
+  e.preventDefault();
 
   emailjs.sendForm('service_0xizr2d', 'template_fqil3b7', e.target, "user_H3Jw8byh3KGDhYIkoNVbM")
     .then((result) => {
@@ -32,7 +32,7 @@ const Contact=()=>{
 
           <Row className="justify-content-md-center py-5 hello">
           <Col xs={12} md={8} lg={8}>
-           <Form onSubmit={handleSubmit(onSubmitHandler)}>
+           <Form onSubmit={onSubmitHandler}>
               <Form.Row className="hello">
                   <Form.Group controlId="formGridName" className="col-sm-12 col-md-6 hello1">
                       <Form.Control
